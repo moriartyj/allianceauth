fa55f8f3e3a3b8fe8f42706b3c6db8f705228f62
Moriarty <moriartyj@gmail.com>
2016-05-23 12:32:40 -0400

    Automatic rejection of old API IDs

--- eveonline/forms.py
+++ eveonline/forms.py
@@ -35,6 +35,8 @@ class UpdateKeyForm(forms.Form):
                     raise forms.ValidationError(u'API key already exist')
                 if EveApiManager.api_key_is_valid(self.cleaned_data['api_id'], self.cleaned_data['api_key']) is False:
                     raise forms.ValidationError(u'API key is invalid')
+                if EveManager.check_if_api_key_pair_is_new(self.cleaned_data['api_id']) is False:
+                    raise forms.ValidationError(u'API key is too old')
                 chars = EveApiManager.get_characters_from_api(self.cleaned_data['api_id'], self.cleaned_data['api_key']).result
                 states = []
                 states.append(self.user_state)
